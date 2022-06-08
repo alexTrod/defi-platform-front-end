225d57711465b3b6b4a73011bffd6c94addaba0d
Alexandra Feldman <a.n.feldman@student.tudelft.nl>
2022-06-08 19:45:39 +0200

    Add front end

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useEthers } from "@usedapp/core";
+import Button from "@mui/material/Button";
+
+export const Header = () => {
+  const { account, activateBrowserWallet, deactivate } = useEthers();
+  const isConnected = account !== undefined;
+
+  return (    
+      <div>
+        {isConnected ? (
+          <Button color="primary" onClick={deactivate} variant="contained">
+            Disconnect
+          </Button>
+        ) : (
+          <Button
+            color="primary"
+            onClick={() => activateBrowserWallet()}
+            variant="contained"
+          >
+            Connect
+          </Button>
+        )}
+    </div>
+  );
+};
---
+++ src/components/main.tsx
@@ -0,0 +1,7 @@
+
+export const Main = () => {
+    // show token values from wallet
+
+    // get address of diff tokens
+    // get balance of users wallet
+}
